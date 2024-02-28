b45ef611b864be472cbd252c1ff4af32d4c5c459
wwtanji <romazotsenko15@gmail.com>
2024-02-28 11:55:44 +0100

    Pridanie Roman Zotsenko do pola data

--- index.php
+++ index.php
@@ -103,7 +103,8 @@ foreach($colors as $color){
 
 $data = array("Peter" => 25,
     "Jana" => 30,
-    "Marek" => 20
+    "Marek" => 20,
+    "Roman Zotsenko" =>17
 );
 
 foreach($data as $name => $age){
